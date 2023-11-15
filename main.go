620198e28149aed75b3d446a57106fd9efe3c49d
SlimNate <slimnatekc@gmail.com>
2023-11-15 13:57:37 -0600

    Add auto-login feature to auth middleware for development

--- main.go
+++ main.go
@@ -17,8 +17,24 @@ import (
 )
 
 const dbFile = "data.db"
+const autoLogin = true
+const autoLoginUser = "admin1"
 
 func AuthMiddleware(sessionRepo *session.SQLiteRepository, userRepo *user.SQLiteRepository) gin.HandlerFunc {
+	// if auto-login is enabled, we skip checking for any session keys
+	// and approve the request as if the `autoLoginUser` is already logged in
+	if autoLogin {
+		return func(ctx *gin.Context) {
+			user, err := userRepo.GetByUsername(autoLoginUser)
+			if err != nil {
+				ctx.AbortWithStatusJSON(500, gin.H{"error": "Error on auto-login, user not found"})
+				return
+			}
+
+			ctx.Set("user", &user.User)
+		}
+	}
+
 	return func(ctx *gin.Context) {
 		sessionKey, err := ctx.Cookie("session_key")
 		if err != nil {
